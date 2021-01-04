0d26adf92a659da1ceb2c2545b100382cc1d2024
zhangdaye <zhangdy123>
2021-01-04 15:28:16 +0800

    zhangdy-mongo

--- src/main/java/com/zhangdy/mongo/config/MongoProxyProcessor.java
+++ src/main/java/com/zhangdy/mongo/config/MongoProxyProcessor.java
@@ -3,9 +3,12 @@ package com.zhangdy.mongo.config;
 
 import com.zhangdy.mongo.config.mongo.MongoProxy;
 import com.zhangdy.mongo.repository.MongoRepository;
+import com.zhangdy.mongo.repository.impl.MongoRepositoryImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.ApplicationContext;
 import org.springframework.core.ResolvableType;
 import org.springframework.stereotype.Component;
 
@@ -14,6 +17,8 @@ import java.lang.reflect.Field;
 @Component
 @Slf4j
 public class MongoProxyProcessor implements BeanPostProcessor {
+    @Autowired
+    private ApplicationContext applicationContext;
 
     private static final String MONGO_REPOSITORY_SIMPLE_NAME = MongoRepository.class.getSimpleName();
     private static final int ZERO = 0;
@@ -27,8 +32,12 @@ public class MongoProxyProcessor implements BeanPostProcessor {
                     field.setAccessible(Boolean.TRUE);
                     ResolvableType type = ResolvableType.forField(field).getGeneric(ZERO);
                     Class<?> clazz = type.resolve();
+                    Object impl = field.get(bean);
+                    if (impl == null) {
+                        impl = applicationContext.getBean(MongoRepositoryImpl.class);
+                    }
                     Object proxy =  MongoProxy.createProxy(field.getType().getClassLoader(),
-                            new Class[]{field.getType()}, field.get(bean), clazz);
+                            new Class[]{field.getType()}, impl, clazz);
                     field.set(bean, proxy);
                 } catch (Exception e) {
                     log.error("bean注入异常 ", e);
